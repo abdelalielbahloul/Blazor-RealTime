05c3be91c35e3ec2d2e2e1cb9c364e746110a7e9
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-03-12 11:12:57 +0100

    feat : send first notification in controller

--- BlazorRealTime.Server/Controllers/NotificationsController.cs
+++ BlazorRealTime.Server/Controllers/NotificationsController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorRealTime.Server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorRealTime.Server.Controllers
 {
@@ -11,5 +13,18 @@ namespace BlazorRealTime.Server.Controllers
     [ApiController]
     public class NotificationsController : ControllerBase
     {
+        private readonly IHubContext<NotificationsHub> _hubContext;
+
+        public NotificationsController(IHubContext<NotificationsHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromQuery] string title)
+        {
+            await _hubContext.Clients.All.SendAsync("notification", $"{DateTime.UtcNow}: {title}");
+            return Ok("notification has been sent successfully!");
+        }
     }
 }
