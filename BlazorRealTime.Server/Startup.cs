76103e63680621facb60bf422dafad99a1185f5b
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-03-12 10:49:18 +0100

    feat : enable cors for any origin and any method and header (is not good but it's just for testing)

--- BlazorRealTime.Server/Startup.cs
+++ BlazorRealTime.Server/Startup.cs
@@ -25,6 +25,13 @@ namespace BlazorRealTime.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("cors", c =>
+                {
+                    c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
             services.AddControllers();
         }
 
